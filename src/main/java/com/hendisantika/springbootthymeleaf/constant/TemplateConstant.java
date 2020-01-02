2a296e9a88f57f366d1edad1a0dc9f6b37340df9
Hendi Santika <hendisantika@gmail.com>
2020-01-02 12:05:20 +0700

    #12 - Add TemplateConstant

---
+++ src/main/java/com/hendisantika/springbootthymeleaf/constant/TemplateConstant.java
@@ -0,0 +1,27 @@
+package com.hendisantika.springbootthymeleaf.constant;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-thymeleaf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/01/20
+ * Time: 12.05
+ */
+public final class TemplateConstant {
+
+    public static final String INDEX = "index";
+    public static final String ALL_USERS = "users";
+    public static final String USER_SHOW = "user-show";
+    public static final String USER_EDIT = "user-edit";
+    public static final String USER_ADD = "user-add";
+
+
+    /**
+     * Private constructor - can not be instantiated
+     */
+    private TemplateConstant() {
+    }
+
+}
