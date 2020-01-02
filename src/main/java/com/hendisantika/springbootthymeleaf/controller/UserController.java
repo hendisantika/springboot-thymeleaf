b3fd6e8d4f74b0f5542f0cffbb8622f8b1acaae7
Hendi Santika <hendisantika@gmail.com>
2020-01-02 12:07:26 +0700

    #16 - Add getUserById in UserController

--- src/main/java/com/hendisantika/springbootthymeleaf/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootthymeleaf/controller/UserController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,5 +33,16 @@ public class UserController {
         model.addAttribute("users", users);
 
         return TemplateConstant.ALL_USERS;
+    }
+
+    @GetMapping(value = "/user-show/{id}")
+    public String getUserById(@PathVariable(value = "id") Integer id, Model model) {
+        Optional<User> userOptional = userService.findById(id);
+        if (userOptional.isPresent()) {
+            model.addAttribute("user", userOptional.get());
+        } else {
+            model.addAttribute("user", Optional.empty());
+        }
+        return TemplateConstant.USER_SHOW;
     }
 }
